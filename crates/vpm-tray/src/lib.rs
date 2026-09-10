8ad4b4240bd4ac0c9814a320eff17d85327f8af7
ikaken <7675846+ikaken@users.noreply.github.com>
2026-09-11 03:04:37 +0900

    Revert "Merge pull request #18 from ikaken/feature/017-portable-mode"
    This reverts commit ab6258623ba0d7bd8c497783231487cd25a96458, reversing
    changes made to 7c0bde02e2c7f3d1daa114c264d0aca4b2639963.

--- crates/vpm-tray/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod tray;
-
-pub const APP_NAME: &str = "VolumeProfileManager";
-pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-pub fn version_string() -> String {
-    format!("v{}", APP_VERSION)
-}
